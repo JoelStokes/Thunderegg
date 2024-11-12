f27c934fd84b8c0d63e70c9ffd975bb19981efe3
jstokes <joel.stokes.e@gmail.com>
2024-11-12 16:02:17 -0500

    Persistant Saving start / Pokemon sprites start / Battle data prep

--- Scripts/BattleController.gd
+++ Scripts/BattleController.gd
@@ -1,12 +1,43 @@
 extends Node2D
 
+var id
+var level
+
+# Json data
 var pokemonFile = "res://Data/pokemon.json"
 var pokemonData
 
-# Called when the node enters the scene tree for the first time.
+# Text Nodes
+var nameNode
+var sizeNode
+var weightNode
+var typeNode
+var levelNode
+var descriptionNode
+var spriteNode
+
 func _ready() -> void:
 	pokemonData = JSON.parse_string(FileAccess.get_file_as_string(pokemonFile))
-	print(pokemonData.get("3").name)
+	
+	#Get ID & Level from previous scene saved info
+	
+	#Set nodes
+	nameNode = $UI/name
+	sizeNode = $UI/size
+	weightNode = $UI/weight
+	typeNode = $UI/type
+	levelNode = $UI/level
+	descriptionNode = $UI/description
+	spriteNode = $Sprite2D
+	
+	#Set text & image
+	nameNode.text = "Name: " + pokemonData.get(id).name
+	sizeNode.text = "Size: " + pokemonData.get(id).size
+	weightNode.text = "Weight: " + pokemonData.get(id).weight
+	typeNode.text = "Type: " + pokemonData.get(id).type
+	levelNode.text = "Level: " + level
+	descriptionNode.text = pokemonData.get(id).description
+	spriteNode.texture = load("res://Sprites/Pokemon/" + id + "front.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ Scripts/SaveController.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+See this for more info:
+	https://docs.godotengine.org/en/stable/tutorials/io/saving_games.html
